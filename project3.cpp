6cc47e78beceaef24b59feb6ed668a02bf44ae29
tanselyn <hannah@Hannahs-MacBook-Pro-2.local>
2015-11-14 23:00:34 -0500

    char* to string

--- project3.cpp
+++ project3.cpp
@@ -23,8 +23,7 @@ int main(int argc, const char * argv[])
     int counter2 = 0;
     
     string line;
-    string parse;
-    char* command;
+    string command;
     
     pair<unordered_map<string, logEntry>::iterator,bool> check;
     
@@ -55,29 +54,30 @@ int main(int argc, const char * argv[])
         
         cout << "% ";
         cin >> command;
-        while (strcmp(command,"q") != 0) {
+        while (command[0] != 'q') {
             
-            if (strcmp(command,"#") == 0) break;
-            else if (strcmp(command,"p") == 0) {
+            if (command[0] == '#') break;
+            
+            else if (command[0] == 'p') == 0) {
                 for (int i = 0; i < excerpt.size(); ++i) {
                     cout << i << "|" << excerpt[i]->first << "|" << excerpt[i]->
                         second.timeStamp << "|" << excerpt[i]->second.category << "|"
                         << excerpt[i]->second.message << '\n';
                 }
             }
-            else if (strcmp(command,"g") == 0) {
+            else if (command[0] == 'g') == 0) {
                 
             }
-            else if (strcmp(command,"l") == 0) {
+            else if (command[0] == 'l') == 0) {
                 excerpt.clear();
                 cout << "excerpt list cleared" << '\n';
             }
-            else if (strcmp(command,"s") == 0) {
+            else if (command[0] == 's') == 0) {
                 
             }
-            else if (strcmp(&command[0],"e") == 0) {
-                char* num = command + 2;
-                counter = atoi(num);
+            else if (command[0] == 'e') == 0) {
+                string parse = command.substr(2);
+                counter = stoi(parse);
                 pair<int,logEntry>* move;
                 if (counter < excerpt.size()) {
                     move = excerpt[counter];
@@ -86,9 +86,9 @@ int main(int argc, const char * argv[])
                     cout << "excerpt list entry " << counter << " moved" << '\n';
                 }
             }
-            else if (strcmp(&command[0],"b") == 0) {
-                char* num = command + 2;
-                counter = atoi(num);
+            else if (command[0] == 'b') == 0) {
+                string parse = command.substr(2);
+                counter = stoi(parse);
                 pair<int,logEntry>* move;
                 if (counter < excerpt.size()) {
                     move = excerpt[counter];
@@ -98,33 +98,33 @@ int main(int argc, const char * argv[])
                 }
                 
             }
-            else if (strcmp(&command[0],"d") == 0) {
-                char* num = command + 2;
-                counter = atoi(num);
+            else if (command[0] == 'd') == 0) {
+                string parse = command.substr(2);
+                counter = stoi(parse);
                 if (counter < excerpt.size()) {
                     excerpt.erase(excerpt.begin() + counter);
                     cout << "excerpt list entry " << counter << " deleted" << '\n';
                 }
             }
-            else if (strcmp(command,"r") == 0) {
+            else if (command[0] == 'r') == 0) {
                 
             }
-            else if (strcmp(&command[0],"a") == 0) {
-                char* num = command + 2;
-                counter = atoi(num);
+            else if (command[0] == 'a') == 0) {
+                string parse = command.substr(2);
+                counter = stoi(parse);
                 if (counter < excerpt.size()) {
                     excerpt.push_back(&masterLog[counter]);
                     cout << "log entry " << counter << " appended" << '\n';
                 }
             }
-            else if (strcmp(&command[0],"k") == 0) {
+            else if (command[0] == 'k') == 0) {
                 
             }
-            else if (strcmp(&command[0],"c") == 0) {
+            else if (command[0] == 'c') == 0) {
                 
                 
             }
-            else if (strcmp(&command[0],"t") == 0) {
+            else if (command[0] == 't') == 0) {
                 
             }
             else cout << "Invalid command. Try again" << '\n';
