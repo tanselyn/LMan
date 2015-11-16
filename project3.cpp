bd350e8e5c36a3f822b4dc48611036251fa6bee2
tanselyn <hannah@Hannahs-MacBook-Pro-2.local>
2015-11-16 15:20:28 -0500

    search implementations

--- project3.cpp
+++ project3.cpp
@@ -10,6 +10,7 @@
 #include <vector>
 #include <cstring>
 #include <algorithm>
+#include <cctype>
 #include "project3.h"
 
 using namespace std;
@@ -21,21 +22,25 @@ int main(int argc, const char * argv[])
     ios_base::sync_with_stdio(false);
     
     bool inOrder = false;
+    bool resultsInOrder = false;
     int counter = 0;
     int counter2 = 0;
     
     string line;
     string command;
-    timeStampCompare comp;
+    timeStampCompare comp1;
+    timeSearchCompare comp2;
     
-    pair<unordered_map<string, logEntry>::iterator,bool> check;
+    pair<unordered_map<string,logEntry>::iterator,bool> check;
     
     vector<pair<int,logEntry>> masterLog;
     vector<pair<int,logEntry>*> excerpt;
-    
-    vector<pair<string,pair<int,logEntry>*>> timeSearchList;
-    
     vector<pair<int,logEntry>*> results;
+    vector<string> keywords;
+    
+    vector<pair<string,logEntry*>> timeSearchList;
+    unordered_map<string,vector<int>> categorySearchList;
+    unordered_map<string,vector<int>> keywordSearchList;
     
     if (strcmp(argv[1],"--help") == 0|| strcmp(argv[1],"-h") == 0) {
         cout << "Use this program to do a variety of search, sort,"
@@ -54,10 +59,58 @@ int main(int argc, const char * argv[])
                 counter2 = line.find('|',counter + 1);
                 next.category = line.substr(counter + 1,counter2 - counter - 1);
                 next.message = line.substr(counter2 + 1,string::npos);
+                for (int i = 0; i < next.category.size(); ++i) {
+                    next.lowerCaseCategory += tolower(next.category[i]);
+                }
+                for (int i = 0; i < next.message.size(); ++i) {
+                    next.lowerCaseMessage += tolower(next.message[i]);
+                }
+                
                 pair<int,logEntry> add(masterLog.size(),next);
                 masterLog.push_back(add);
+                timeSearchList.push_back(pair<string,logEntry*>(next.timeStamp,
+                                    &masterLog[masterLog.size() - 1].second));
+                
+                unordered_map<string,vector<int>>::iterator location
+                    = categorySearchList.find(next.lowerCaseCategory);
+                if (location != categorySearchList.end()) {
+                    location->second.push_back(masterLog.size() - 1);
+                }
+                else {
+                    categorySearchList[next.lowerCaseCategory];
+                }
+                string word;
+                for (int i = 0; i < next.lowerCaseCategory.size(); ++i) {
+                    if (!isalnum(next.lowerCaseCategory[i])) {
+                        if (!word.empty()) {
+                            location = keywordSearchList.find(word);
+                            if (location != keywordSearchList.end()) {
+                                location->second.push_back(masterLog.size() - 1);
+                            }
+                            else keywordSearchList[word];
+                            word.clear();
+                        }
+                    }
+                    else word += next.lowerCaseCategory[i];
+                }
+                
+                for (int i = 0; i < next.lowerCaseMessage.size(); ++i) {
+                    if (!isalnum(next.lowerCaseMessage[i])) {
+                        if (!word.empty()) {
+                            location = keywordSearchList.find(word);
+                            if (location != keywordSearchList.end()) {
+                                location->second.push_back(masterLog.size() - 1);
+                            }
+                            else keywordSearchList[word];
+                            word.clear();
+                        }
+                    }
+                    else word += next.lowerCaseMessage[i];
+                }
             }
         }
+        
+        sort(timeSearchList.begin(),timeSearchList.end(),comp2);
         
         cout << "% ";
         getline(cin,command);
@@ -73,17 +126,29 @@ int main(int argc, const char * argv[])
                 }
             }
             else if (command[0] == 'g') {
-                
+                if (!results.empty()) {
+                    if (resultsInOrder == false) {
+                        sort(results.begin(),results.end(),comp1);
+                        resultsInOrder = true;
+                    }
+                    for (int i = 0; i < results.size(); ++i) {
+                        cout << results[i]->first << "|" << results[i]->
+                        second.timeStamp << "|" << results[i]->second.category << "|"
+                        << results[i]->second.message << '\n';
+                    }
+                }
             }
             else if (command[0] == 'l') {
-                inorder = false;
+                inOrder = false;
                 excerpt.clear();
                 cout << "excerpt list cleared" << '\n';
             }
             else if (command[0] == 's') {
-                sort(excerpt.begin(),excerpt.end(),comp);
-                cout << "excerpt list sorted" << '\n';
-                inOrder = true;
+                if (inOrder == false) {
+                    sort(excerpt.begin(),excerpt.end(),comp1);
+                    cout << "excerpt list sorted" << '\n';
+                    inOrder = true;
+                }
             }
             else if (command[0] == 'e') {
                 inOrder = false;
@@ -119,7 +184,15 @@ int main(int argc, const char * argv[])
                 }
             }
             else if (command[0] == 'r') {
-                
+                if (!results.empty()) {
+                    if (resultsInOrder == false) {
+                        sort(results.begin(),results.end(),comp1);
+                        resultsInOrder = true;
+                    }
+                    for (int i = 0; i < results.size(); ++i) {
+                        excerpt.push_back(results[i]);
+                    }
+                    cout << results.size() << " log entries appended" << '\n';
             }
             else if (command[0] == 'a') {
                 inOrder = false;
@@ -131,29 +204,85 @@ int main(int argc, const char * argv[])
                 }
             }
             else if (command[0] == 'k') {
+                resultsInOrder = false;
+                bool resultsExist = true;
+                results.clear();
+                counter2 = 0;
+                string parse = command.substr(2);
+                string word;
+                for (int i = 0; i < parse.size(); ++i) {
+                    if (!isalnum(parse[i])) {
+                        if (!word.empty()) {
+                            keywords.push_back(word);
+                            word.clear();
+                        }
+                    }
+                    else word += parse[i];
+                }
+                vector<int> v(1);
+                vector<int>::iterator intersection = v.begin();
+                for (int i = 0; i < keywords.size(); i += 2) {
+                    unordered_map<string,vector<int>>::iterator location1 =
+                            keywordSearchList.find(keywords[i]);
+                    unordered_map<string,vector<int>>::iterator location2 =
+                            keywordSearchList.find(keywords[i + 1]);
+                    if (location1 == keywordSearchList.end() || location2 ==
+                        keywordSearchList.end()) {
+                        resultsExist = false;
+                        break;
+                    }
+                    else {
+                        intersection = (location1->second.begin(),location1->second.end(),
+                                        location2->second.begin(),location2->second.end(),
+                                        intersection);
+                    }
+                }
+                if (resultsExist) {
+                    while (!v.empty()) {
+                        counter2 = v.size();
+                        for (int i = 0; i < v.size(); ++i) {
+                            results.push_back(&masterLog[v[1]]);
+                        }
+                    }
+                }
+                resultsInOrder = true;
                 
+                cout << counter2 << " entries found" << '\n';
             }
             else if (command[0] == 'c') {
+                resultsInOrder = false;
+                results.clear();
+                counter2 = 0;
+                string parse = command.substr(2);
+                unordered_map<string,vector<int>>::iterator location = categorySearchList.find(parse);
+                if (location != categorySearchList.end()) {
+                    counter2 = location->second.size();
+                }
+                for (int i = 0; i < location->second.size(); ++i) {
+                    results.push_back(&masterLog[location->second[i]]);
+                }
+                
+                cout << counter2 << " entries found" << '\n';
                 
                 
             }
             else if (command[0] == 't') {
+                results.clear();
                 counter2 = 0;
                 string parse = command.substr(2);
                 counter = parse.find('|',0);
                 string start = parse.substr(0,counter);
                 string end = parse.substr(counter + 1,string::npos);
-                if (inOrder == false) {
-                    sort(excerpt.begin(),excerpt.end(),comp);
-                    cout << "excerpt list sorted" << '\n';
-                    inOrder = true;
-                }
-                for (int i = 0; i < excerpt.size(); ++i) {
-                    if (excerpt[i]->second.timeStamp >= start &&
-                        excerpt[i]->second.timeStamp < end) {
-                        ++counter2;
-                        results.push_back(excerpt[i]);
-                    }
+
+                vector<pair<string,int>>::iterator lower = lower_bound(timeSearchList.begin(),
+                                                    timeSearchList.end(), start);
+                
+                vector<pair<string,int>>::iterator upper = lower_bound(timeSearchList.begin(),
+                                                    timeSearchList.end(), end);
+                while (lower != upper) {
+                    results.push_back(&masterLog[lower->second]);
+                    ++lower;
+                    ++counter2;
                 }
                 cout << counter2 << " entries found" << '\n';
                 
--- project3.h
+++ project3.h
@@ -15,18 +15,23 @@ struct logEntry {
     std::string timeStamp;
     std::string category;
     std::string message;
+    std::string lowerCaseCategory;
+    std::string lowerCaseMessage;
     
-    logEntry(): timeStamp("a"), category("a"), message("a") {}
+    logEntry(): timeStamp("a"), category("a"), message("a"),
+                lowerCaseCategory("a"), lowerCaseMessage("a") {}
     
     void operator=(const logEntry &other) {
         timeStamp = other.timeStamp;
         category = other.category;
         message = other.message;
+        lowerCaseCategory = other.lowerCaseCategory;
+        lowerCaseMessage = other.lowerCaseMessage;
     }
 };
 
 struct timeStampCompare {
-    bool operator() (std::pair<int,logEntry>* &rhs, std::pair<int,logEntry>* &lhs) const {
+    bool operator() (std::pair<int,logEntry>* rhs, std::pair<int,logEntry>* lhs) const {
         if (rhs->second.timeStamp == lhs->second.timeStamp) {
             if (rhs->second.category == lhs->second.category) {
                 return rhs->first < lhs->first;
@@ -37,5 +42,17 @@ struct timeStampCompare {
     }
 };
 
+struct timeSearchCompare {
+    bool operator() (std::pair<std::string,logEntry*> &rhs,
+                     std::pair<std::string,logEntry*> &lhs) const {
+        if (rhs.first == lhs.first) {
+            if (rhs.second->category == lhs.second->category) {
+                return rhs.second < lhs.second;
+            }
+            else return rhs.second->category < lhs.second->category;
+        }
+        else return rhs.second->timeStamp < lhs.second->timeStamp;
+    }
+};
 
 #endif
